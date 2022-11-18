b06fa606dcd50c6bf2cb7f51a200793f333be0a1
ThomasPDM <thomas.pelissie@laposte.net>
2022-11-18 12:56:39 +0100

    distinguish epoch from step

--- mlp.py
+++ mlp.py
@@ -5,12 +5,12 @@ import torch
 
 ### Set hyperparameters
 LR = 0.1 # learning rate
-EPOCHS = 100000 # amount of forward/backward propagations
+EPOCHS = 20000 # amount of forward/backward propagations
 BATCH_SIZE = 2 # amount of backproped data per epoch
 
 
 ### Load Data
-torch.manual_seed(42)
+torch.manual_seed(871)
 X = torch.Tensor([[0.1, 0.3, 0.1, 0.6, 0.4, 0.6, 0.5, 0.9, 0.4, 0.7],
                   [0.1, 0.4, 0.5, 0.9, 0.2, 0.3, 0.6, 0.2, 0.4, 0.6]]).T
 y = torch.nn.functional.one_hot(torch.LongTensor(
@@ -30,27 +30,27 @@ b3 = torch.rand((1,2))
 loss = torch.empty(size=(EPOCHS,))
 
 for epoch in tqdm(range(EPOCHS), "Train"):
-    # Forward propagation
-    ids = torch.randperm(X.shape[0])[:BATCH_SIZE] # batch indices
-    a0 = X[ids, :]
-    a1 = torch.sigmoid(a0@w1 + b1)
-    a2 = torch.sigmoid(a1@w2 + b2)
-    a3 = torch.sigmoid(a2@w3 + b3)
-    loss[epoch] = torch.mean((a3 - y[ids])**2).detach()/BATCH_SIZE
+    for ids in torch.split(torch.randperm(X.shape[0]), BATCH_SIZE):
+        # Forward propagation
+        a0 = X[ids, :]
+        a1 = torch.sigmoid(a0@w1 + b1)
+        a2 = torch.sigmoid(a1@w2 + b2)
+        a3 = torch.sigmoid(a2@w3 + b3)
+        loss[epoch] = torch.mean((a3 - y[ids])**2).detach()/BATCH_SIZE
 
-    # Backward propagation
-    da3 = a3*(1-a3)*(a3-y[ids,:])/BATCH_SIZE
-    da2 = a2*(1-a2)*(da3@w3.T)
-    da1 = a1*(1-a1)*(da2@w2.T)
-    da0 = a0*(1-a0)*(da1@w1.T)
+        # Backward propagation
+        da3 = a3*(1-a3)*(a3-y[ids,:])/BATCH_SIZE
+        da2 = a2*(1-a2)*(da3@w3.T)
+        da1 = a1*(1-a1)*(da2@w2.T)
+        da0 = a0*(1-a0)*(da1@w1.T)
 
-    # Update parameters
-    w3 -= LR*a2.T@da3
-    w2 -= LR*a1.T@da2
-    w1 -= LR*a0.T@da1
-    b3 -= LR*da3.sum(dim=0)
-    b2 -= LR*da2.sum(dim=0)
-    b1 -= LR*da1.sum(dim=0)
+        # Update parameters
+        w3 -= LR*a2.T@da3
+        w2 -= LR*a1.T@da2
+        w1 -= LR*a0.T@da1
+        b3 -= LR*da3.sum(dim=0)
+        b2 -= LR*da2.sum(dim=0)
+        b1 -= LR*da1.sum(dim=0)
 
 
 ### Results
